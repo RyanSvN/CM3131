1ecd956255302001f6cb2a349478d3926383c2d9
RyanSvN <ryanstevenson7522@gmail.com>
2023-03-28 17:10:26 +0100

    Ingredients Flagging

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-
 // Scanner will be initialized in DOM inside element with id of 'reader'
 const scanner = new Html5QrcodeScanner('reader', { 
     // Sets dimensions of scanning box (set relative to reader element width)
@@ -12,12 +11,13 @@ const scanner = new Html5QrcodeScanner('reader', {
 });
 
 
-// Starts scanner
+// Starts Scanner
 scanner.render(success, error);
 
 
 async function success(result) {
 
+    //API URL & OPTIONS
     const api_url = 'https://edamam-food-and-grocery-database.p.rapidapi.com/parser?upc=' + result;
 
     const options = {
@@ -28,8 +28,21 @@ async function success(result) {
         }
     };
 
+    //Allergy Array
+    var allergyWords = ['Milk', 'Peanuts', 'Mustard'];
+
+    //Fetch API Data
     const response = await fetch(api_url, options);
     const data = await response.json();
+
+    //Give the ingredients data variable 'ingredientsElement'
+    var ingredientsElement = data.hints[0].food.foodContentsLabel;
+    
+    //Compare arrays and flag matching allergy words
+    for (var i = 0; i < allergyWords.length; i++) {
+        var regex = new RegExp(allergyWords[i], 'gi');
+        ingredientsElement = ingredientsElement.replace(regex, '<span id="highlight">' + allergyWords[i] + '</span>');
+    }
 
 
 
@@ -37,7 +50,7 @@ async function success(result) {
     <h1>Success!</h1>
     <h2>${data.hints[0].food.label}</h2>
     <img src="${data.hints[0].food.image}" alt="Error: No Image Available"></img>
-    <p>${data.hints[0].food.foodContentsLabel}</p>
+    <p>${ingredientsElement}</p>
     `
     // Prints result as a link inside result element
 
