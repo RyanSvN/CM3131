85eecc06cf19cbbcb6dc74abbf31936d41db39e5
RyanSvN <ryanstevenson7522@gmail.com>
2023-04-10 16:15:44 +0100

    New Repo

---
+++ prefscript.js
@@ -0,0 +1,21 @@
+let boxes = document.getElementsByClassName('box').length;
+
+//saving
+function save() {
+    for(let i = 1; i <= boxes; i++) {
+        var checkbox = document.getElementById(String(i));
+        localStorage.setItem("checkbox" + String(i), checkbox.checked);
+    }
+}
+
+//loading
+for (let i = 1; i <= boxes; i++) {
+    if(localStorage.length > 0) {
+        var checked = JSON.parse(localStorage.getItem("checkbox" + String(i)));
+        if (checked == true) {
+            document.getElementById(String(i)).checked = checked;
+        }
+    }
+}
+window.addEventListener('change', save);
+console.log('test'); 
